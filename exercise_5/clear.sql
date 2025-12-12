6074b8ec49f3ae96f6293f8fc6779824b762f32b
Alessio Saltarin <alessiosaltarin@gmail.com>
2025-12-12 12:32:47 +0100

    Added clear

---
+++ exercise_5/clear.sql
@@ -0,0 +1,6 @@
+-- PostgreSQL script to drop all tables from exercise_2
+
+-- Drop tables in reverse order of dependencies
+DROP TABLE IF EXISTS Author CASCADE;
+DROP TABLE IF EXISTS Post CASCADE;
+DROP TABLE IF EXISTS Comment CASCADE;
