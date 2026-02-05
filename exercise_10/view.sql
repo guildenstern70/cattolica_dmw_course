8fe9ec3fbac6170febb21350fb79179c0ad60b67
Alessio Saltarin <alessiosaltarin@gmail.com>
2026-02-05 10:55:57 +0100

    New view on exercise 10

--- exercise_10/view.sql
+++ exercise_10/view.sql
@@ -1,24 +1,16 @@
-CREATE VIEW View_Top_Students AS
-WITH MaxMarks AS (
-    SELECT 
-        c.title AS course_title,
-        s.faculty AS student_faculty,
-        s.name,
-        s.surname,
-        e.mark,
-        e.date,
-        -- Rank students by mark within each course
-        RANK() OVER (PARTITION BY e.cod_course ORDER BY e.mark DESC) as mark_rank
-    FROM Exam e
-    JOIN Student s ON e.student_id = s.student_id
-    JOIN Course c ON e.cod_course = c.cod_course
-)
+CREATE VIEW StudentStudyPlanView AS
 SELECT 
-    course_title,
-    student_faculty,
-    name,
-    surname,
-    mark,
-    date
-FROM MaxMarks
-WHERE mark_rank = 1;
+    S.Surname AS StudentSurname, 
+    C.Title AS CourseTitle, 
+    P.Surname AS ProfessorSurname, 
+    E.Mark
+FROM 
+    StudyPlan SP
+JOIN 
+    Student S ON SP.StudentId = S.StudentId
+JOIN 
+    Course C ON SP.CourseId = C.CourseId
+JOIN 
+    Professor P ON C.Professor = P.ProfessorId
+LEFT JOIN 
+    Exam E ON SP.StudentId = E.StudentId AND SP.CourseId = E.CourseId;
