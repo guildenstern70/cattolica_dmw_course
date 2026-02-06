f143234b84c08163d0c9f6e144aa9132c44a0e26
Alessio Saltarin <alessiosaltarin@gmail.com>
2026-02-06 16:39:21 +0100

    Changed output of exercise 9

--- exercise_9/select.sql
+++ exercise_9/select.sql
@@ -2,11 +2,12 @@ SELECT
     a.Title AS "Album Title",
     ac."Order" AS "Song Order",
     t.Title AS "Song Name",
-    s.Name || ' ' || s.Surname AS "Singer"
+    STRING_AGG(s.Name || ' ' || s.Surname, ', ' ORDER BY s.Surname ASC) AS "Singers"
 FROM ALBUM a
 JOIN ALBUM_CONTENT ac ON a.AlbumId = ac.AlbumId
 JOIN TRACK t ON ac.TrackId = t.TrackId
 JOIN SINGER_TRACK st ON t.TrackId = st.TrackId
 JOIN SINGER s ON st.SingerId = s.SingerId
-WHERE a.AlbumId = (SELECT MIN(AlbumId) FROM ALBUM) -- Select the first album
-ORDER BY ac."Order" ASC, s.Surname ASC;
+WHERE a.AlbumId = (SELECT MIN(AlbumId) FROM ALBUM)
+GROUP BY a.Title, ac."Order", t.Title
+ORDER BY ac."Order" ASC;
